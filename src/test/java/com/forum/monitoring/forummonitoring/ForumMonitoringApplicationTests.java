eeb5b658ae7863a61973993130f69aadfa44a9ca
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2022-01-24 21:34:52 -0300

    first commit

---
+++ src/test/java/com/forum/monitoring/forummonitoring/ForumMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.monitoring.forummonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
